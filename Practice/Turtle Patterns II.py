effac37b8584d7c2fc367ab17b3d80ac51c8b14f
josh_rockwood <joshrockwood@hotmail.com>
2024-03-04 21:10:12 -0700

    Colors/Scale/CommandLine

--- Practice/Turtle Patterns II.py
+++ Practice/Turtle Patterns II.py
@@ -1,53 +1,104 @@
+import sys
 import turtle
 
 
 def main():
-    global t
     t = turtle.Turtle()
     t.speed(5)
 
-    create_house()
-    create_tractor()
+    command = int(sys.argv[1])
+
+    if command == 1:
+        create_house(t, scale_factor = 1.5, main_color = 'red')
+        create_tractor(t, scale_factor = 0.8)
+    elif command == 2:
+        create_house(t, scale_factor = 1)
+        create_tractor(t, scale_factor = 1)
+    elif command == 3:
+        create_house(t, scale_factor = 0.8)
+        create_tractor(t, scale_factor = 1.5, main_color = 'orange')
 
 
-def create_house():
-    draw_rectangle(100, 100)
-    reposition_turtle(100, 100)
-    draw_triangle(100, 120)
-    reposition_turtle(40, 0)
-    draw_rectangle(40, 20)
-    reposition_turtle(10, 50)
-    draw_rectangle(20, 20)
-    reposition_turtle(70, 50)
-    draw_rectangle(20, 20)
-
-
-def create_tractor():
+def create_house(t, scale_factor = 1.0, main_color = 'burlywood'):
     # Main box
-    reposition_turtle(-200, 0)
-    draw_rectangle(50, 100)
+    t.fillcolor(main_color)
+    t.begin_fill()
+    draw_rectangle(t, 100 * scale_factor, 100 * scale_factor)
+    t.end_fill()
+    # Roof
+    reposition_turtle(t, 100 * scale_factor, 100 * scale_factor)
+    t.fillcolor('dark gray')
+    t.begin_fill()
+    draw_triangle(t, 100 * scale_factor, 120)
+    t.end_fill()
+    # Door
+    reposition_turtle(t, 40 * scale_factor, 0 * scale_factor)
+    t.fillcolor('cornflower blue')
+    t.begin_fill()
+    draw_rectangle(t, 40 * scale_factor, 20 * scale_factor)
+    t.end_fill()
+    # Windows
+    reposition_turtle(t, 10 * scale_factor, 50 * scale_factor)
+    t.fillcolor('alice blue')
+    t.begin_fill()
+    draw_rectangle(t, 20 * scale_factor, 20 * scale_factor)
+    t.end_fill()
+    reposition_turtle(t, 70 * scale_factor, 50 * scale_factor)
+    t.fillcolor('alice blue')
+    t.begin_fill()
+    draw_rectangle(t, 20 * scale_factor, 20 * scale_factor)
+    t.end_fill()
+
+
+def create_tractor(t, scale_factor = 1.0, main_color = 'green'):
+    # Main box
+    reposition_turtle(t, -200 * scale_factor, 0 * scale_factor)
+    t.fillcolor(main_color)
+    t.begin_fill()
+    draw_rectangle(t, 50 * scale_factor, 100 * scale_factor)
+    t.end_fill()
     # Cabin
-    reposition_turtle(-140, 50)
-    draw_rectangle(40, 40)
-    reposition_turtle(-135, 55)
-    draw_rectangle(30, 15)
-    reposition_turtle(-145, 90)
-    draw_rectangle(5, 50)
+    reposition_turtle(t, -140 * scale_factor, 50 * scale_factor)
+    t.begin_fill()
+    draw_rectangle(t, 40 * scale_factor, 40 * scale_factor)
+    t.end_fill()
+    reposition_turtle(t, -135 * scale_factor, 55 * scale_factor)
+    t.fillcolor('alice blue')
+    t.begin_fill()
+    draw_rectangle(t, 30 * scale_factor, 15 * scale_factor)
+    t.end_fill()
+    reposition_turtle(t, -145 * scale_factor, 90 * scale_factor)
+    t.fillcolor('silver')
+    t.begin_fill()
+    draw_rectangle(t, 5 * scale_factor, 50 * scale_factor)
+    t.end_fill()
     # Spout
-    reposition_turtle(-190, 50)
-    draw_rectangle(30, 10)
+    reposition_turtle(t, -190 * scale_factor, 50 * scale_factor)
+    t.begin_fill()
+    draw_rectangle(t, 30 * scale_factor, 10 * scale_factor)
+    t.end_fill()
     # Wheels
-    reposition_turtle(-190, -15)
-    draw_circle(25)
-    reposition_turtle(-120, -15)
-    draw_circle(30)
-    reposition_turtle(-190, -3)
-    draw_circle(12.5)
-    reposition_turtle(-120, -3)
-    draw_circle(17)
+    reposition_turtle(t, -190 * scale_factor, -15 * scale_factor)
+    t.fillcolor('black')
+    t.begin_fill()
+    draw_circle(t, 25 * scale_factor)
+    t.end_fill()
+    reposition_turtle(t, -120 * scale_factor, -15 * scale_factor)
+    t.begin_fill()
+    draw_circle(t, 30 * scale_factor)
+    t.end_fill()
+    reposition_turtle(t, -190 * scale_factor, -3 * scale_factor)
+    t.fillcolor('yellow')
+    t.begin_fill()
+    draw_circle(t, 12.5 * scale_factor)
+    t.end_fill()
+    reposition_turtle(t, -120 * scale_factor, -3 * scale_factor)
+    t.begin_fill()
+    draw_circle(t, 17 * scale_factor)
+    t.end_fill()
 
 
-def draw_rectangle(height, width):
+def draw_rectangle(t, height, width):
     for i in range(2):
         t.forward(width)
         t.lt(90)
@@ -55,17 +106,17 @@ def draw_rectangle(height, width):
         t.lt(90)
 
 
-def draw_triangle(length, angle):
+def draw_triangle(t, length, angle):
     for i in range(3):
         t.lt(angle)
         t.forward(length)
 
 
-def draw_circle(radius):
+def draw_circle(t, radius):
     t.circle(radius)
 
 
-def reposition_turtle(x, y):
+def reposition_turtle(t, x, y):
     t.penup()
     t.goto(x, y)
     t.setheading(0)
